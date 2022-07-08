f0aed0d14ad2bb3ac4f2af8817f872e65c98555f
Hector Ocampo <hector2603@users.noreply.github.com>
2022-07-08 17:24:22 -0500

    finish get recommendation with collaborative fiultering

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-
 PROVIDERS_CSV = 'datasets/providers.csv'
 MODEL_COLLABORATIVE_FILTERING = 'datasets/collaborativeFilteringModelVersion.csv'
 DATASETS_NEW_MOVIES_CSV = 'datasets/newMovies.csv'
 RATINGS_CSV = 'datasets/newRatings.csv'
+FULL_RATINGS_CSV = 'datasets/ratings.csv'
+MOVIES_IDS_CSV = 'datasets/movieIds.csv'
 PATH_MODELS = 'models/'
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
@@ -25,36 +25,98 @@ def hello_world():
 
 @app.route('/collaborativeFiltering/getRecommendationsForUserId/<id>', methods=['GET'])
 def collaborativeFiltering_getRecommendationsForUSerId(id):
-
     new_rating_file_pd = pd.read_csv(RATINGS_CSV)
     new_rating_file_pd = new_rating_file_pd.astype({"userId": int}, errors='raise')
     new_rating_file_pd = new_rating_file_pd.astype({"movieId": int}, errors='raise')
     new_rating_file_pd = new_rating_file_pd.astype({"timestamp": int}, errors='raise')
+
+    new_movies_file_pd = pd.read_csv(MOVIES_IDS_CSV)
+    movie_ids = new_movies_file_pd["movieId"].unique().tolist()
+    movie2movie_encoded = {x: i for i, x in enumerate(movie_ids)}
+    movie_encoded2movie = {i: x for i, x in enumerate(movie_ids)}
+
+    version_model_collaborative_filtering_pd = pd.read_csv(MODEL_COLLABORATIVE_FILTERING)
+    last_version_name = version_model_collaborative_filtering_pd['file_name'][
+        version_model_collaborative_filtering_pd.index[-1]]
+    num_users = version_model_collaborative_filtering_pd['num_users'][
+        version_model_collaborative_filtering_pd.index[-1]]
+    num_movies = version_model_collaborative_filtering_pd['num_movies'][
+        version_model_collaborative_filtering_pd.index[-1]]
+
+    model = RecommenderNet(num_users, num_movies, 50)
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate=0.00005))
+    model.build((17500067, 2))
+    model.load_weights(PATH_MODELS + last_version_name, by_name=False, skip_mismatch=False, options=None)
+
+    movies_watched_by_user = new_rating_file_pd[new_rating_file_pd.userId == int(id)]
+    movies_not_watched = new_movies_file_pd[~new_movies_file_pd["movieId"].isin(movies_watched_by_user.movieId.values)][
+        "movieId"]
+    movies_not_watched = list(set(movies_not_watched).intersection(set(movie2movie_encoded.keys())))
+    movies_not_watched = [[movie2movie_encoded.get(x)] for x in movies_not_watched]
+    user_movie_array = np.hstack(([[int(id) - 1]] * len(movies_not_watched), movies_not_watched))
+
+    ratings = model.predict(user_movie_array).flatten()
+    top_ratings_indices = ratings.argsort()[-10:][::-1]
+    recommended_movie_ids = [
+        movie_encoded2movie.get(movies_not_watched[x][0]) for x in top_ratings_indices
+    ]
+
+    recommendation_dict = {"recommendedMovies": recommended_movie_ids}
+    return recommendation_dict
+
+
+@app.route('/collaborativeFiltering/trainModel/', methods=['GET'])
+def collaborativeFiltering_trainModel():
+    new_rating_file_pd = pd.read_csv(RATINGS_CSV)
+    new_rating_file_pd = new_rating_file_pd.astype({"userId": int}, errors='raise')
+    new_rating_file_pd = new_rating_file_pd.astype({"movieId": int}, errors='raise')
+    new_rating_file_pd = new_rating_file_pd.astype({"timestamp": int}, errors='raise')
+
+    full_rating_file_pd = pd.read_csv(FULL_RATINGS_CSV)
+    full_rating_file_pd = full_rating_file_pd.astype({"userId": int}, errors='raise')
+    full_rating_file_pd = full_rating_file_pd.astype({"movieId": int}, errors='raise')
+    full_rating_file_pd = full_rating_file_pd.astype({"timestamp": int}, errors='raise')
+
+    full_rating_file_pd = pd.concat([new_rating_file_pd, full_rating_file_pd])
+
+    user_ids = full_rating_file_pd["userId"].unique().tolist()
+    user2user_encoded = {x: i for i, x in enumerate(user_ids)}
+    userencoded2user = {i: x for i, x in enumerate(user_ids)}
 
     new_movies_file_pd = pd.read_csv(DATASETS_NEW_MOVIES_CSV)
     movie_ids = new_movies_file_pd["movieId"].unique().tolist()
     movie2movie_encoded = {x: i for i, x in enumerate(movie_ids)}
     movie_encoded2movie = {i: x for i, x in enumerate(movie_ids)}
 
-    version_model_collaborative_filtering_pd = pd.read_csv(MODEL_COLLABORATIVE_FILTERING)
-    last_version_name = version_model_collaborative_filtering_pd['file_name'][version_model_collaborative_filtering_pd.index[-1]]
-    num_users = version_model_collaborative_filtering_pd['num_users'][version_model_collaborative_filtering_pd.index[-1]]
-    num_movies = version_model_collaborative_filtering_pd['num_movies'][version_model_collaborative_filtering_pd.index[-1]]
+    full_rating_file_pd["user"] = full_rating_file_pd["userId"].map(user2user_encoded)
+    full_rating_file_pd["movie"] = full_rating_file_pd["movieId"].map(movie2movie_encoded)
+
+    num_users = len(user2user_encoded)
+    num_movies = len(movie_encoded2movie)
+
+    full_rating_file_pd["rating"] = full_rating_file_pd["rating"].values.astype(np.float32)
+    min_rating = min(full_rating_file_pd["rating"])
+    max_rating = max(full_rating_file_pd["rating"])
+
+    df = full_rating_file_pd.sample(frac=1, random_state=42)
+    x = df[["user", "movie"]].values
+    y = df["rating"].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values
+    train_indices = int(0.7 * df.shape[0])
+    x_train, x_val, y_train, y_val = (
+        x[:train_indices],
+        x[train_indices:],
+        y[:train_indices],
+        y[train_indices:],
+    )
 
     model = RecommenderNet(num_users, num_movies, 50)
-    model.compile(loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate=0.00005))
-    model.build((17500067, 2))
-    model.load_weights(PATH_MODELS+last_version_name, by_name=False, skip_mismatch=False, options=None)
+    model.compile(
+        loss=tf.keras.losses.BinaryCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate=0.00005)
+    )
+    model.build(x_train.shape)
+    history = model.fit(x=x_train, y=y_train, batch_size=1024, epochs=10, verbose=2, validation_data=(x_val, y_val))
 
-    movies_watched_by_user = new_rating_file_pd[new_rating_file_pd.userId == int(id)]
-    movies_not_watched = new_movies_file_pd[~new_movies_file_pd["movieId"].isin(movies_watched_by_user.movieId.values)]["movieId"]
-    movies_not_watched = list(set(movies_not_watched).intersection(set(movie2movie_encoded.keys())))
-    movies_not_watched = [[movie2movie_encoded.get(x)] for x in movies_not_watched]
-    user_movie_array = np.hstack(([[int(id)-1]] * len(movies_not_watched), movies_not_watched))
-
-    ratings = model.predict(user_movie_array).flatten()
-
-    return str(ratings)
+    return history
 
 
 @app.route('/addRaiting', methods=['POST'])
@@ -117,4 +179,4 @@ def add_watch_provider_for_movie():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0' , debug=True)
